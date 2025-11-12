55c27451952ecbe976bea8a7872a78dfa89f2084
Thanaphat7306 <thanaphat.ss7306@gmail.com>
2025-11-12 18:02:25 +0700

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,31 @@
+export default function DashboardPage() {
+  return (
+    <>
+        <div className="bg-gray-300 flex min- h-screen  ">
+<aside className="w-64 bg-white p-8 ">
+            <h1 className="text-4xl font-bold mb-8">YourLogo</h1>
+            <div className="text-gray-700 text-lg hover:text-blue-700 cursor-pointer m-4">Dashboard</div>
+            <div className="text-gray-700 text-lg hover:text-blue-700 cursor-pointer m-4">Users</div>
+            <div className="text-gray-700 text-lg hover:text-blue-700 cursor-pointer m-4">Document</div>
+            <div className="text-gray-700 text-lg hover:text-blue-700 cursor-pointer m-4">Photos</div>
+            <div className="text-gray-700 text-lg hover:text-blue-700 cursor-pointer m-4">Hierarchy</div>
+            <br />
+            <div className="text-gray-700 text-lg hover:text-blue-700 cursor-pointer m-4">Message</div>
+            <div className="text-gray-700 text-lg hover:text-blue-700 cursor-pointer m-4">Help</div>
+            <div className="text-gray-700 text-lg hover:text-blue-700 cursor-pointer m-4">Setting</div>
+        </aside>
+        <div className="flex-1 p-8">
+            <div >
+                <h2 className="text-2xl font-bold ">Hello, Lekan</h2>
+                <p className="text-gray-700">Have a nice day</p>
+            </div>
+        </div >
+            
+        </div>
+    </>
+    
+
+   
+    
+  )
+}
